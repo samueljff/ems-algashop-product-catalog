8a7024516e1b5273525a656dc4c0428069992f43
samueljff <samueljosef3@gmail.com>
2026-04-17 15:51:11 -0300

    Testando respostas de erro padrão RFC 7807

---
+++ src/contractTest/resources/contracts/product/createProductV1BadRequest.groovy
@@ -0,0 +1,34 @@
+package contracts.product
+
+
+import org.springframework.cloud.contract.spec.Contract
+
+Contract.make {
+    request {
+        method POST()
+        headers {
+            accept 'application/json'
+            contentType 'application/json'
+        }
+        urlPath("/api/v1/products") {
+            body([
+                    name: value("")
+            ])
+        }
+
+    }
+
+    response {
+        status(400)
+        headers {
+            contentType 'application/problem+json'
+        }
+
+        body([
+               instance: fromRequest().path(),
+               type: "/errors/invalid-fields",
+               title: "Invalid fields",
+               detail: "One or more fields are invalid"
+        ])
+    }
+}
