017a584a56797d25e6df1217ee347f4a01145ba2
samueljff <samueljosef3@gmail.com>
2026-05-10 16:41:34 -0300

    Aprimorando respostas da API de Customer

--- src/main/java/com/fonseca/algashop/product/catalog/ProductCatalogApplication.java
+++ src/main/java/com/fonseca/algashop/product/catalog/ProductCatalogApplication.java
@@ -3,11 +3,15 @@ package com.fonseca.algashop.product.catalog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class ProductCatalogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductCatalogApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        SpringApplication.run(ProductCatalogApplication.class, args);
+    }
 
 }
