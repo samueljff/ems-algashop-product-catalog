8f0261e2fddb0830aa5a8a25aa464042673c9b5b
samueljff <samueljosef3@gmail.com>
2026-06-21 01:14:19 -0300

    Integração com Fastpay

--- src/test/java/com/fonseca/algashop/product/catalog/ProductCatalogApplicationTests.java
+++ src/test/java/com/fonseca/algashop/product/catalog/ProductCatalogApplicationTests.java
@@ -1,10 +1,23 @@
 package com.fonseca.algashop.product.catalog;
 
+import com.fonseca.algashop.product.catalog.application.category.managment.CategoryManagementService;
+import com.fonseca.algashop.product.catalog.application.category.query.CategoryQueryService;
+import com.fonseca.algashop.product.catalog.application.product.query.ProductQueryService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class ProductCatalogApplicationTests {
+
+	@MockitoBean
+	private CategoryQueryService categoryQueryService;
+
+	@MockitoBean
+	private CategoryManagementService categoryManagementService;
+
+	@MockitoBean
+	private ProductQueryService productQueryService;
 
 	@Test
 	void contextLoads() {
