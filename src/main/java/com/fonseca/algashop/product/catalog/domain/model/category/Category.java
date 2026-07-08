0d728496926f527efe9f87aecd6c4931b642f5d1
samueljff <samueljosef3@gmail.com>
2026-07-07 22:37:05 -0300

    Mapeando Domain Entity como Document

---
+++ src/main/java/com/fonseca/algashop/product/catalog/domain/model/category/Category.java
@@ -0,0 +1,50 @@
+package com.fonseca.algashop.product.catalog.domain.model.category;
+
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.OffsetDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+@Document(collection = "categories")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Category {
+
+    @Id
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    private String name;
+
+    private Boolean enabled;
+
+    private OffsetDateTime createdAt;
+
+    private OffsetDateTime updatedAt;
+
+    public Category(String name, Boolean enabled) {
+        this.setName(name);
+        this.setEnabled(enabled);
+        this.createdAt = OffsetDateTime.now();
+    }
+
+    public void setName(String name) {
+        if (StringUtils.isBlank(name)) {
+            throw new IllegalArgumentException();
+        }
+        this.name = name;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        Objects.requireNonNull(enabled);
+        this.enabled = enabled;
+    }
+}
