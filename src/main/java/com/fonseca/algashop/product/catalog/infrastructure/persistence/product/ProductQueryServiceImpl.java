f671a6b9124e51125f12538703f6f3078cc8be89
samueljff <samueljosef3@gmail.com>
2026-07-19 18:10:39 -0300

    Filtros com Criteria e MongoOperations (MongoTemplate)

--- src/main/java/com/fonseca/algashop/product/catalog/infrastructure/persistence/product/ProductQueryServiceImpl.java
+++ src/main/java/com/fonseca/algashop/product/catalog/infrastructure/persistence/product/ProductQueryServiceImpl.java
@@ -9,11 +9,17 @@ import com.fonseca.algashop.product.catalog.domain.model.product.Product;
 import com.fonseca.algashop.product.catalog.domain.model.product.ProductNotFoundException;
 import com.fonseca.algashop.product.catalog.domain.model.product.ProductRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -21,6 +27,7 @@ public class ProductQueryServiceImpl implements ProductQueryService {
 
     private final ProductRepository productRepository;
     private final Mapper mapper;
+    private final MongoOperations mongoOperations;
 
     @Override
     public ProductDetailOutput findById(UUID productId) {
@@ -30,8 +37,46 @@ public class ProductQueryServiceImpl implements ProductQueryService {
 
     @Override
     public PageModel<ProductSummaryOutput> filter(ProductFilter filter) {
-        Page<Product> products = productRepository.findAll(PageRequest.of(filter.getPage(), filter.getSize()));
-        Page<ProductSummaryOutput> productOutputs = products.map(p -> mapper.convert(p, ProductSummaryOutput.class));
-        return PageModel.of(productOutputs);
+        Query query = queryWith(filter);
+
+        long totalItems = mongoOperations.count(query, Product.class);
+
+        Sort sort = sortWith(filter);
+
+        PageRequest pageRequest = PageRequest.of(filter.getPage(), filter.getSize(), sort);
+
+        Query pagedQuery = query.with(pageRequest);
+        List<Product> products;
+        int totalPages = 0;
+        if (totalItems > 0) {
+            products = mongoOperations.find(pagedQuery, Product.class);
+            totalPages = (int) Math.ceil((double) totalItems / pageRequest.getPageSize());
+        } else {
+            products = new ArrayList<>();
+        }
+        List<ProductSummaryOutput> productOutputs = products.stream()
+            .map(p -> mapper.convert(p, ProductSummaryOutput.class)).collect(Collectors.toList());
+        return PageModel.<ProductSummaryOutput>builder()
+            .content(productOutputs)
+            .number(pageRequest.getPageNumber())
+            .size(pageRequest.getPageSize())
+            .totalElements(totalItems)
+            .totalPages(totalPages)
+            .build();
+    }
+
+    private Sort sortWith(ProductFilter filter) {
+
+        return Sort.by(filter.getSortDirectionOrDefault(), filter.getSortByPropertyOrDefault().getPropertyName());
+    }
+
+    private Query queryWith(ProductFilter filter) {
+        Query query = new Query();
+
+        if (filter.getEnabled() != null) {
+            query.addCriteria(Criteria.where("enabled").is(filter.getEnabled()));
+        }
+
+        return query;
     }
 }
