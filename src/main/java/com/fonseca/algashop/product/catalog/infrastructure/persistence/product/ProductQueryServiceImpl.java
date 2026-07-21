9312a486dd7be59152c4421842b79e6824922563
samueljff <samueljosef3@gmail.com>
2026-07-20 23:32:07 -0300

    Comparando dois campos no mesmo documento

--- src/main/java/com/fonseca/algashop/product/catalog/infrastructure/persistence/product/ProductQueryServiceImpl.java
+++ src/main/java/com/fonseca/algashop/product/catalog/infrastructure/persistence/product/ProductQueryServiceImpl.java
@@ -12,6 +12,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.aggregation.AggregationExpressionCriteria;
+import org.springframework.data.mongodb.core.aggregation.ComparisonOperators;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
@@ -98,6 +100,18 @@ public class ProductQueryServiceImpl implements ProductQueryService {
                 query.addCriteria(Criteria.where("salePrice").gte(filter.getPriceFrom()));
             }  else if (filter.getPriceTo() != null) {
                 query.addCriteria(Criteria.where("salePrice").lte(filter.getPriceTo()));
+            }
+        }
+
+        if (filter.getHasDiscount() != null) {
+            if (filter.getHasDiscount()) {
+                query.addCriteria(AggregationExpressionCriteria.whereExpr(
+                    ComparisonOperators.valueOf("$salePrice").lessThan("$regularPrice")
+                ));
+            }else {
+                query.addCriteria(AggregationExpressionCriteria.whereExpr(
+                    ComparisonOperators.valueOf("$salePrice").equalTo("$regularPrice")
+                ));
             }
         }
 
