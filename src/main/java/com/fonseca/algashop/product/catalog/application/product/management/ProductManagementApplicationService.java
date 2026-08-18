7414b27c34d100720798965ae280b5334efe3232
samueljff <samueljosef3@gmail.com>
2026-08-18 00:19:51 -0300

    Transações com Spring e MongoDB

--- src/main/java/com/fonseca/algashop/product/catalog/application/product/management/ProductManagementApplicationService.java
+++ src/main/java/com/fonseca/algashop/product/catalog/application/product/management/ProductManagementApplicationService.java
@@ -7,6 +7,7 @@ import com.fonseca.algashop.product.catalog.domain.model.product.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
@@ -60,12 +61,14 @@ public class ProductManagementApplicationService {
         productRepository.save(product);
     }
 
+    @Transactional
     public void restock(UUID productId, int quantity){
         Product product = findProduct(productId);
         StockMovement movement = stockService.restock(product, quantity);
         stockMovementRepository.save(movement);
     }
 
+    @Transactional
     public void withdraw(UUID productId, int quantity){
         Product product = findProduct(productId);
         StockMovement movement = stockService.withdraw(product, quantity);
