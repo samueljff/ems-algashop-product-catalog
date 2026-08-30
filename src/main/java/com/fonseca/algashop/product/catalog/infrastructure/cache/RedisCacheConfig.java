0b381eb190b9046c76694a5c18e42d57c44d403c
samueljff <samueljosef3@gmail.com>
2026-08-30 17:00:36 -0300

    Invalidando cache via TTL

--- src/main/java/com/fonseca/algashop/product/catalog/infrastructure/cache/RedisCacheConfig.java
+++ src/main/java/com/fonseca/algashop/product/catalog/infrastructure/cache/RedisCacheConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 
+import java.time.Duration;
+
 @Configuration
 @EnableCaching
 @ConditionalOnProperty(name = "spring.cache.type", havingValue = "redis")
@@ -16,8 +18,10 @@ public class RedisCacheConfig {
     @Bean
     public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
         RedisCacheConfiguration defaultCacheConfig = RedisCacheConfiguration.defaultCacheConfig()
-            .computePrefixWith(c -> c + ":");
+            .computePrefixWith(c -> c + ":")
+            .entryTtl(Duration.ofMinutes(1));
         return (builder) ->builder.cacheDefaults(defaultCacheConfig)
-            .withCacheConfiguration("algashop:products:v1", defaultCacheConfig.disableCachingNullValues());
+            .withCacheConfiguration("algashop:products:v1",
+                defaultCacheConfig.disableCachingNullValues().entryTtl(Duration.ofMinutes(5)));
     }
 }
