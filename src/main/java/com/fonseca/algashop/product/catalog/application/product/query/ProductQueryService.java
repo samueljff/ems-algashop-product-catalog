88995f553a6952d45464f5773c861311c7d8f581
samueljff <samueljosef3@gmail.com>
2026-08-30 16:32:58 -0300

    Implementando Server-side cache com Cache-Aside

--- src/main/java/com/fonseca/algashop/product/catalog/application/product/query/ProductQueryService.java
+++ src/main/java/com/fonseca/algashop/product/catalog/application/product/query/ProductQueryService.java
@@ -1,11 +1,13 @@
 package com.fonseca.algashop.product.catalog.application.product.query;
 
 import com.fonseca.algashop.product.catalog.application.utility.PageModel;
+import org.springframework.cache.annotation.Cacheable;
 
 import java.util.UUID;
 
 public interface ProductQueryService {
 
+    @Cacheable(cacheNames = "algashop:products:v1", key = "#productId")
     ProductDetailOutput findById(UUID productId);
     PageModel<ProductSummaryOutput> filter(ProductFilter filter);
 }
