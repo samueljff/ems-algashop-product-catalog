b4fc67182210a447c9ac600dd676c244e769c562
samueljff <samueljosef3@gmail.com>
2026-09-08 14:14:21 -0300

    Executando o Cache antes do Retry

--- src/main/java/com/fonseca/algashop/product/catalog/presentation/ProductController.java
+++ src/main/java/com/fonseca/algashop/product/catalog/presentation/ProductController.java
@@ -58,7 +58,7 @@ public class ProductController {
     @GetMapping("/{productId}")
     public ResponseEntity<ProductDetailOutput> findBYId(@PathVariable UUID productId) {
 
-        if (Math.random() < 0.3) {
+        if (Math.random() < 0.6) {
             try {
                 Thread.sleep(Duration.ofSeconds(20));
             } catch (Exception e) {
