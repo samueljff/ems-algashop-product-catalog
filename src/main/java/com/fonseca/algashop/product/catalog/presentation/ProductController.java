75ef72df72cc5a78cd466aebf857d4922e160454
samueljff <samueljosef3@gmail.com>
2026-09-07 21:24:52 -0300

    Utilizando o Retry Pattern com @Retryable

--- src/main/java/com/fonseca/algashop/product/catalog/presentation/ProductController.java
+++ src/main/java/com/fonseca/algashop/product/catalog/presentation/ProductController.java
@@ -58,7 +58,7 @@ public class ProductController {
     @GetMapping("/{productId}")
     public ResponseEntity<ProductDetailOutput> findBYId(@PathVariable UUID productId) {
 
-        if (Math.random() < 0.5) {
+        if (Math.random() < 0.8) {
             try {
                 Thread.sleep(Duration.ofSeconds(20));
             } catch (Exception e) {
